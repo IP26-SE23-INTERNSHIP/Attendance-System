5a6962b0506347ea071f71e811cd4e900b12d25f
HerambLimaye <heramba96l@gmail.com>
2021-07-14 01:28:05 +0530

    timestamp implemented json

--- recognizer.py
+++ recognizer.py
@@ -1,16 +1,33 @@
 ''''
 Real Time Face Recogition
-	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc
 	==> LBPH computed model (trained faces) should be on trainer/ dir
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition
 
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 
 '''
 
 import cv2
 import numpy as np
-import os 
+import os
+import json
+from datetime import date,datetime
+from pathlib import Path
+
+
+p = open('user.json', 'r')
+users = json.load(p)
+
+names = []
+roll=[]
+dct = {}
+
+
+
+for k, v in users.items():
+    names.append(v['name'].split()[0])
+    roll.append(k)
 
 recognizer = cv2.face.FisherFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -19,59 +36,95 @@ faceCascade = cv2.CascadeClassifier(cascadePath);
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
+# iniciate id counter
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = [ 'Nimish', 'Bhumi','Heramba']
+# names = [ 'Nimish', 'Bhumi','Heramba']
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
 
 # Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
 
+xyz=-1
+ctr=0
 while True:
 
-    ret, img =cam.read()
+    ret, img = cam.read()
     # img = cv2.flip(img, -1) # Flip vertically
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale( 
+    faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
 
-    for(x,y,w,h) in faces:
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+    for (x, y, w, h) in faces:
 
-        id, confidence = recognizer.predict(cv2.resize(gray[y:y+h,x:x+w],(250,250)),)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Check if confidence is less them 100 ==> "0" is perfect match 
+        id, confidence = recognizer.predict(cv2.resize(gray[y:y + h, x:x + w], (250, 250)), )
+
+
+        # Check if confidence is less them 100 ==> "0" is perfect match
         if (confidence < 600):
+
+            if xyz==id:
+                ctr+=1
+            else :
+                ctr=0
+            xyz=id
             id = names[id]
             confidence = "  {0}%".format(round(confidence))
+            print(ctr)
+
+
+
+            if ctr>100:
+                myfile = Path('./attendance/' + str(date.today()) + '.json')
+                myfile.touch(exist_ok=True)
+                try :
+                    jsonf=open('./attendance/'+str(date.today())+'.json', 'r', encoding='utf-8')
+
+                    jsonfcheck=json.load(jsonf)
+
+                    if roll[xyz] not in jsonfcheck.keys():
+                        jsonfcheck[roll[xyz]]=users[roll[xyz]]
+                        jsonfcheck[roll[xyz]]['time'] = str(datetime.now())
+                        jsonf=open('./attendance/'+str(date.today())+'.json', 'w', encoding='utf-8')
+                        jsonf.write(json.dumps(jsonfcheck, indent=4))
+                        jsonf.close()
+                except json.decoder.JSONDecodeError:
+                    dct[roll[xyz]] = users[roll[xyz]]
+                    dct[roll[xyz]]['time'] = str(datetime.now())
+                    jsonf = open('./attendance/' + str(date.today()) + '.json', 'w', encoding='utf-8')
+                    jsonf.write(json.dumps(dct, indent=4))
+                    jsonf.close()
+
         else:
             id = "unknown"
             confidence = "  {0}%".format(round(confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
 
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', img)
+
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
+
 cam.release()
 cv2.destroyAllWindows()
