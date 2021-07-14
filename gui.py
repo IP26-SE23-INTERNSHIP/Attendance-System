7479d761162d7c57c6698b194120d1504e68f941
realhunter7869 <samant.nimish@gmail.com>
2021-07-14 23:15:10 +0530

    GUI

--- gui.py
+++ gui.py
@@ -5,6 +5,27 @@ import os
 from datetime import datetime
 import json
 from datetime import date
+import webbrowser
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import subprocess
+
+
+# class Serv(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         if self.path == '/':
+#             self.path = '/attendance.html'
+#         try:
+#             file_to_open = open(self.path[1:]).read()
+#             self.send_response(200)
+#         except:
+#             file_to_open = "File not found"
+#             self.send_response(404)
+#         self.end_headers()
+#         self.wfile.write(bytes(file_to_open, 'utf-8'))
+#
+#
+# httpd = HTTPServer(('localhost', 7800), Serv)
+# httpd.serve_forever()
 
 
 def hellocallback():
@@ -73,7 +94,11 @@ def function6():
 
 
 def attend():
-    os.startfile(os.getcwd() + "attendance.html")
+    # os.system("python custom_server.py 1")
+    subprocess.call('start /wait python -m http.server 7800', shell=True)
+    # httpd = HTTPServer(('localhost', 7800), Serv)
+    # httpd.serve_forever()
+    # os.startfile(os.getcwd() + "attendance.html")
 
 
 # stting title for the window
